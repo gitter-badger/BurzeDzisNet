3db335672fe0df65a8d9898a85272128ed9dea11
Krzysztof Piasecki <krzysiekpiasecki@gmail.com>
2015-09-11 20:19:32 +0200

    Refactor Storm

--- src/BurzeDzisNet/Storm.php
+++ src/BurzeDzisNet/Storm.php
@@ -8,23 +8,21 @@ namespace BurzeDzisNet;
 
 /**
  * Storm represents information about registered lightnings with a specified radius of monitoring
- * covered by the given location
+ * covered by the specified location
  *
  * Storm has properties describing:
  * - number (number of cloud-to-ground lightning in specified radius from a selected location)
  * - distance (distance to the nearest registered lightning in km)
  * - direction (direction to the nearest lightning [E, E, N, NW, W, SW, S, SE])
  * - period (number of minutes of time period containing the data [10, 15, 20 minutes])
- * - location (selected location)
  * - radius (radius covered by Point in km)
  *
  * @author Krzysztof Piasecki <krzysiekpiasecki@gmail.com>
  */
 class Storm
 {
-
     /**
-     * The number of cloud-to-ground lightning in specified radius from a selected location
+     * The number of cloud-to-ground lightning in specified radius from a specified location
      *
      * @var int number of minutes of time period containing the data
      */
@@ -52,18 +50,11 @@ class Storm
     protected $period = 0;
 
     /**
-     * The radius covered by location
+     * The radius covered by specified location
      *
-     * @var int radius covered by location
+     * @var int radius covered by specified location
      */
     protected $radius = 0;
-
-    /**
-     * Selected location
-     *
-     * @var Point selected location
-     */
-    protected $location = null;
 
     /**
      * New Storm
@@ -73,26 +64,14 @@ class Storm
      * @param string $direction direction to the nearest lightning (E, E, N, NW, W, SW, S, SE)
      * @param int $period number of minutes of time period containing the data (10, 15, 20 minutes)
      * @param int $radius radius covered by location
-     * @param Point $location selected location
      */
-    public function __construct($number, $distance, $direction, $period, $radius, Point $location)
+    public function __construct($number, $distance, $direction, $period, $radius)
     {
         $this->number = $number;
         $this->distance = $distance;
         $this->direction = $direction;
         $this->period = $period;
         $this->radius = $radius;
-        $this->location = $location;
-    }
-
-    /**
-     * Get selected location
-     *
-     * @return Point Selected location
-     */
-    public function getLocation()
-    {
-        return $this->location;
     }
 
     /**
@@ -138,14 +117,12 @@ class Storm
     }
 
     /**
-     * Get radius covered by Point
+     * Get radius covered by specified location
      *
-     * @see getLocation seletected location
      * @return int radius covered by location
      */
     public function getRadius()
     {
         return $this->radius;
-    }
-
+    }phpi
 }
