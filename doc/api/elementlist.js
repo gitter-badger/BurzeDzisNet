06b8c99a16fe7e8cbf9d2b687ed40dde58e360ac
Krzysztof Piasecki <krzysiekpiasecki@gmail.com>
2015-09-13 23:11:21 +0200

    Fix apigen config and regenarate api

--- doc/api/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","BurzeDzisNet\\Alert"],["c","BurzeDzisNet\\BurzeDzisNet"],["c","BurzeDzisNet\\BurzeDzisNetInterface"],["c","BurzeDzisNet\\Endpoint"],["c","BurzeDzisNet\\EndpointInterface"],["c","BurzeDzisNet\\Point"],["c","BurzeDzisNet\\Storm"],["c","BurzeDzisNet\\WeatherAlert"]];
