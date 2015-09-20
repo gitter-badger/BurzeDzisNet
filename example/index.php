03e553c6b41aca216ed55034eb3bbfdc47614d13
Krzysztof Piasecki <krzysiekpiasecki@gmail.com>
2015-09-20 20:15:37 +0200

    Disable WSDL caching in example app

--- example/index.php
+++ example/index.php
@@ -4,9 +4,10 @@ namespace BurzeDzisNet;
 
 require_once "../vendor/autoload.php";
 
-define("API_KEY", "");
+define("API_KEY", "4d36bcb5c4f892dbc042f3e1b0972d4ce686b479");
 define("MY_LOCATION", "Budapest");
 define("MONITORING_RADIUS", 250);
+
 
 try {
 
@@ -16,7 +17,9 @@ try {
     }
 
     $burzeDzisNet = new BurzeDzisNet(
-        new Endpoint(\API_KEY)
+        new Endpoint(\API_KEY, [
+            "cache_wsdl" => \WSDL_CACHE_NONE
+        ])
     );
 
     if ($burzeDzisNet->verifyApiKey(API_KEY) === false) {
@@ -32,6 +35,7 @@ try {
     require_once "layout.html";
 
 } catch (\Exception $e) {
+    var_dump($e);
     \error_log($e);
     throw $e;
 }
