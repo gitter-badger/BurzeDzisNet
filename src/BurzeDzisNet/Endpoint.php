4358d536c2d4a8e1f8a59f3071532686b82656cc
Krzysztof Piasecki <krzysiekpiasecki@gmail.com>
2015-09-20 11:47:58 +0200

    Add custom options to soap endpoint

--- src/BurzeDzisNet/Endpoint.php
+++ src/BurzeDzisNet/Endpoint.php
@@ -32,25 +32,37 @@ class Endpoint implements EndpointInterface
     private $apiKey = "";
 
     /**
+     * Custom options
+     *
+     * @var array custom options
+     */
+    private $options = [];
+
+    /**
      * Constructor
      *
+     * Custom options will be used as an argument when calling {@see getClient} method.
+     *
      * @param string $apiKey API key
+     * @param array $options custom soap options
      */
-    public function __construct($apiKey)
+    public function __construct($apiKey, array $options = [])
     {
         $this->apiKey = $apiKey;
+        $this->options = $options;
     }
 
     /**
      * Get Soap client in WSDL mode
      *
+     * @see __construct() Customizing client with option parameter
      * @see {@link \SoapClient http://php.net/manual/en/class.soapclient.php} SoapClient
      * @return SoapClient SoapClient in WSDL mode
      * @throws SoapFault if the WSDL URI cannot be loaded or parsed
      */
     public function getClient()
     {
-        return new SoapClient($this->getWSDL());
+        return new SoapClient($this->getWSDL(), $this->options);
     }
 
     /**
